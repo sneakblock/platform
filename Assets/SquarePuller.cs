a880b79dcc8aab34d76b65a9d91924ee144f7201
block3 <block3@gatech.edu>
2021-11-14 11:58:59 -0500

    brad

--- Assets/SquarePuller.cs
+++ Assets/SquarePuller.cs
@@ -8,7 +8,10 @@ public class SquarePuller : MonoBehaviour
 
     private GameObject[] otherSquares;
 
-    public int attractForce;
+    public Color StandardColor;
+    public Color ToColor;
+
+    public float attractForce;
 
     [Range(1, 60)]
     public float minPullFuse;
@@ -22,27 +25,65 @@ public class SquarePuller : MonoBehaviour
     [Range(5, 60)]
     public float maxPullRate;
 
+    [Range(1, 10)]
+    public float pullDuration;
+
+    [Range(0, 5)]
+    public float riseRate;
+
     private float pullFuse;
     private float pullRate;
+    private Vector3 riseRateVector3;
 
 
 
     // Start is called before the first frame update
     void Start()
     {
+        GetComponent<SpriteRenderer>().color = StandardColor;
         pullFuse = Random.Range(minPullFuse, maxPullFuse);
         pullRate = Random.Range(minPullRate, maxPullRate);
-        otherSquares = GameObject.FindGameObjectsWithTag("square");
-        InvokeRepeating("PullSqaures", pullFuse, pullRate);
+        riseRateVector3 = new Vector3(0, riseRate, 0);
+        InvokeRepeating("PullSquares", pullFuse, pullRate);
     }
 
     void PullSquares()
     {
-        for (int i = 0; i < otherSquares.Length; i++)
-        {
-            Rigidbody2D squareRB = otherSquares[i].GetComponent<Rigidbody2D>();
-            squareRB.AddForce((otherSquares[i].transform.position - transform.position) * attractForce);
-        }
+        Debug.Log("Pulling squares.");
+        otherSquares = GameObject.FindGameObjectsWithTag("square");
+        StartCoroutine(Puller());
+        StartCoroutine(ColorChange());
+        StartCoroutine(Kinematicer());
+    }
 
+    IEnumerator Puller()
+    {
+
+        float timer = 0;
+        while (timer < pullDuration)
+        {
+            transform.position += riseRateVector3;
+            for (int i = 0; i < otherSquares.Length; i++)
+            {
+                otherSquares[i].GetComponent<Rigidbody2D>().AddForce((transform.position - otherSquares[i].transform.position) * attractForce);
+            }
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        
+    }
+
+    IEnumerator ColorChange()
+    {
+        GetComponent<SpriteRenderer>().color = ToColor;
+        yield return new WaitForSeconds(pullDuration);
+        GetComponent<SpriteRenderer>().color = StandardColor;
+    }
+
+    IEnumerator Kinematicer()
+    {
+        GetComponent<Rigidbody2D>().isKinematic = true;
+        yield return new WaitForSeconds(pullDuration);
+        GetComponent<Rigidbody2D>().isKinematic = false;
     }
 }
